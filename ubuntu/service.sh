4fe3371d074224928cb311a43d13b7868b27a2d7
yjjnls <x-jj@foxmail.com>
2018-12-04 09:25:58 +0800

    update apt-mirror folder

--- ubuntu/service.sh
+++ ubuntu/service.sh
@@ -1,2 +1,8 @@
 #! /bin/bash
+sudo apt install -y apache2
+
+sudo mkdir /var/www/html/ubuntu
+sudo ln -s /dev/data/ubuntu/16.04/mirror/mirrors.aliyun.com/ubuntu/ /var/www/html/ubuntu/16.04
+sudo ln -s /dev/data/ubuntu/18.04/mirror/mirrors.aliyun.com/ubuntu/ /var/www/html/ubuntu/18.04
+
 cp -f /etc/apt/sources.list  /etc/apt/sources.list.bak 

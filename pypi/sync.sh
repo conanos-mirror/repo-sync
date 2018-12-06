db5c66bddb85d5779d246bf21a8dee1e144e328c
yjjnls <x-jj@foxmail.com>
2018-12-06 09:36:19 +0800

    add sudo authority

--- pypi/sync.sh
+++ pypi/sync.sh
@@ -1,19 +1,20 @@
 #! /bin/bash
-rm -rf pip2pi
-git clone https://github.com/yjjnls/pip2pi.git
+sudo pip3 uninstall pip2pi
+sudo rm -rf pip2pi
+sudo git clone https://github.com/yjjnls/pip2pi.git
 cd pip2pi
-python3 setup.py install
+sudo python3 setup.py install
 cd -
 
 
-mkdir -p /dev/data/pypi/packages
+sudo mkdir -p /dev/data/pypi/packages
 
 # pypi-server -p 8080 -P . -a . /dev/data/pypi/packages
 
-pip2tgz /dev/data/pypi/packages -r requirements.txt
+sudo pip2tgz /dev/data/pypi/packages -r requirements.txt
 
 # pip2tgz /dev/data/pypi/packages cam
 
-dir2pi /dev/data/pypi/packages
+sudo dir2pi /dev/data/pypi/packages
 
 sudo ln -s /dev/data/pypi/packages/simple /var/www/html/pypi
